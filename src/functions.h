793b961ee37991d13dc7c0d251709e836b8f2359
Kasai Kou <streamwest1629@gmail.com>
2023-01-05 18:17:17 +0900

    #19: cv::Matの変換関数をヘッダーファイルに追加

--- src/functions.h
+++ src/functions.h
@@ -7,4 +7,9 @@
 
 using ErrorMsg = std::string;
 
+cv::Mat to_cvmat(ARGB* image, const int& width, const int& height, const bool& monochrome = false) noexcept;
+cv::Mat to_cvmat(ARGB* image, const int& width, const int& height, const RectArea& padding, const bool& monochrome = false) noexcept;
+void assign_cvmat(ARGB* image, const int& width, const int& height, cv::Mat mat);
+
+
 ErrorMsg opencv_binalization(ARGB* image, const int& width, const int& height, const RGB& dark, const RGB& blight, const int& mode);
