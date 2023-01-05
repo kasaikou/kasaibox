ac1658710225b9a11eb3f361faf99d9a0fa78e90
Kasai Kou <streamwest1629@gmail.com>
2023-01-05 10:18:07 +0900

    #5: ncnnについて使用バイナリを修正

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -3,11 +3,11 @@ Set-Location $PSScriptRoot\..
 New-Item .\downloads -ItemType Directory
 
 # ncnn
-Invoke-WebRequest -Uri "https://github.com/Tencent/ncnn/releases/download/20221128/ncnn-20221128-windows-vs2022.zip" -OutFile .\downloads\ncnn-windows.zip
+Invoke-WebRequest -Uri "https://github.com/Tencent/ncnn/releases/download/20221128/ncnn-20221128-windows-vs2022-shared.zip" -OutFile .\downloads\ncnn-windows.zip
 Expand-Archive .\downloads\ncnn-windows.zip -DestinationPath .\downloads\ncnn-windows -Force
-Copy-Item -Path .\downloads\ncnn-windows\ncnn-20221128-windows-vs2022\x86\bin -Destination .\libs -Recurse -Force
-Copy-Item -Path .\downloads\ncnn-windows\ncnn-20221128-windows-vs2022\x86\include -Destination .\libs -Recurse -Force
-Copy-Item -Path .\downloads\ncnn-windows\ncnn-20221128-windows-vs2022\x86\lib -Destination .\libs -Recurse -Force
+Copy-Item -Path .\downloads\ncnn-windows\ncnn-20221128-windows-vs2022-shared\x86\bin -Destination .\libs -Recurse -Force
+Copy-Item -Path .\downloads\ncnn-windows\ncnn-20221128-windows-vs2022-shared\x86\include -Destination .\libs -Recurse -Force
+Copy-Item -Path .\downloads\ncnn-windows\ncnn-20221128-windows-vs2022-shared\x86\lib -Destination .\libs -Recurse -Force
 
 # lua
 Invoke-WebRequest -Uri "https://master.dl.sourceforge.net/project/luabinaries/5.1.4/Windows%20Libraries/lua5_1_4_Win32_dll8_lib.zip?viasf=1" -OutFile .\downloads\lua-windows.zip
