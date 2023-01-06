3894f1bfc88ae47dc24ce55d6a678263d991ed5c
Kasai Kou <streamwest1629@gmail.com>
2023-01-07 08:34:38 +0900

    #19: WIP: とりあえず実装してみる

--- src/functions.h
+++ src/functions.h
@@ -2,10 +2,17 @@
 
 #include "type.h"
 #include <string>
+#include <map>
+#include <memory>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <ncnn/net.h>
 
 using ErrorMsg = std::string;
+ErrorMsg no_error = "";
+ErrorMsg make_error(const std::string& msg) {
+	return msg;
+}
 
 cv::Mat color_to_cvmat(ARGB* image, const int& width, const int& height, const bool& monochrome = false) noexcept;
 cv::Mat color_to_cvmat(ARGB* image, const int& width, const int& height, const RectArea& padding, const bool& monochrome = false) noexcept;
---
+++ src/real_esrgan.cpp
@@ -0,0 +1,163 @@
+#include "functions.h"
+
+std::pair<cv::Mat, ErrorMsg> inference(const cv::Mat image, const ncnn::Net& model);
+
+cv::Mat to_ocv(const ncnn::Mat& result) {
+
+	cv::Mat cv_result32f = cv::Mat::zeros(result.w, result.h, CV_32FC3);
+	cv::Mat cv_result8b;
+
+	for (int y = 0; y < result.h; ++y)
+		for (int x = 0; x < result.w; ++x) {
+			cv_result32f.at<cv::Vec3f>(x, y)[0] = result.channel(2)[x + result.w * y];
+			cv_result32f.at<cv::Vec3f>(x, y)[1] = result.channel(1)[x + result.w * y];
+			cv_result32f.at<cv::Vec3f>(x, y)[2] = result.channel(0)[x + result.w * y];
+		}
+
+	cv_result32f.convertTo(cv_result8b, CV_8UC3, 255.0, 0.0);
+	return cv_result8b;
+}
+
+ErrorMsg real_esrgan(ARGB* image, const int& width, const int& height, const RectArea& extend, const std::string& modelpath_noext) {
+
+	if (width != 4 * (width - (extend.left + extend.right))) {
+		return "invalid width: extended != 4x origin";
+	}
+	else if (height != 4 * (height - (extend.top + extend.bottom))) {
+		return "invalid height: extended != 4x origin";
+	}
+
+	auto origin_color = color_to_cvmat(image, width, height, extend);
+	auto origin_alpha = just_alpha_to_cvmat(image, width, height, extend);
+
+	// load model
+	static std::map<std::string, std::shared_ptr<ncnn::Net>> models = {};
+	std::shared_ptr<ncnn::Net> net;
+	if (models.contains(modelpath_noext)) {
+		net = models[modelpath_noext];
+	}
+	else {
+		net = std::make_shared<ncnn::Net>();
+		auto res = net->load_param((modelpath_noext + ".param").c_str());
+		if (res < 0) {
+			return "open .param file failed";
+		}
+
+		res = net->load_model((modelpath_noext + ".bin").c_str());
+		if (res < 0) {
+			return "open .bin file failed";
+		}
+
+		net->opt.use_vulkan_compute = false;
+		net->opt.num_threads = 4;
+
+		models.insert(std::make_pair(modelpath_noext, net));
+	}
+
+	// inference
+	auto result_color = inference(origin_color, *net.get());
+	if (result_color.second != "") {
+		return result_color.second;
+	}
+	auto result_alpha = inference(origin_alpha, *net.get());
+	if (result_color.second != "") {
+		return result_color.second;
+	}
+
+	for (int i = 0, l = width * height; i < l; ++i) {
+		image[i].a = std::byte(0);
+		image[i].r = std::byte(0);
+		image[i].g = std::byte(0);
+		image[i].b = std::byte(0);
+	}
+
+	for (int y = 0, h = min(height, min(result_color.first.rows, result_alpha.first.rows)); y < h; ++y)
+		for (int x = 0, w = min(width, min(result_color.first.cols, result_alpha.first.cols)); x < w; ++x) {
+			image[x + y * width].b = std::byte(result_color.first.at<cv::Vec3b>(x, y)[0]);
+			image[x + y * width].g = std::byte(result_color.first.at<cv::Vec3b>(x, y)[1]);
+			image[x + y * width].r = std::byte(result_color.first.at<cv::Vec3b>(x, y)[2]);
+			int alpha = 1;
+			alpha += result_alpha.first.at<cv::Vec3b>(x, y)[0];
+			alpha += result_alpha.first.at<cv::Vec3b>(x, y)[1];
+			alpha += result_alpha.first.at<cv::Vec3b>(x, y)[2];
+			image[x + y * width].a = std::byte(alpha / 3);
+		}
+
+	return "";
+}
+
+std::pair<cv::Mat, ErrorMsg> inference(const cv::Mat image, const ncnn::Net& model) {
+	
+	const int tile_size = 200;
+	const int tile_padding = 10;
+	const int scale = 4;
+
+	cv::Mat padding_image;
+
+	// pre-process
+	int padding_image_w = image.cols % 2, padding_image_h = image.rows % 2;
+	cv::copyMakeBorder(image, padding_image, 0, padding_image_w, 0, padding_image_h, cv::BORDER_CONSTANT, cv::Scalar(0));
+
+	cv::Mat result(padding_image.cols * scale, padding_image.rows * scale, CV_8UC3);
+
+	// tile-splitted compute
+	for (int y_tile = 0, h_tile = std::ceil((float)padding_image.rows / tile_size); y_tile < h_tile; ++y_tile)
+		for (int x_tile = 0, w_tile = std::ceil((float)padding_image.cols / tile_size); x_tile < w_tile; ++x_tile) {
+
+			// input rect area
+			int input_top = y_tile * tile_size;
+			int input_bottom = min(input_top + tile_size, padding_image.rows);
+			int input_left = x_tile * tile_size;
+			int input_right = min(input_left + tile_size, padding_image.cols);
+
+			// input-padding rect area
+			int input_top_padding = max(input_top - tile_padding, 0);
+			int input_bottom_padding = min(input_bottom - tile_padding, padding_image.rows);
+			int input_left_padding = max(input_left - tile_padding, 0);
+			int input_right_padding = min(input_bottom - tile_padding, padding_image.cols);
+
+			int input_tile_w = input_right - input_left;
+			int input_tile_h = input_bottom - input_top;
+			int input_tile_padding_w = input_right_padding - input_left_padding;
+			int input_tile_padding_h = input_bottom_padding - input_top_padding;
+
+			cv::Mat input_tile = padding_image(cv::Rect(input_left_padding, input_top_padding, input_tile_padding_w, input_tile_padding_h)).clone();
+
+			// inference
+			const float norm_vals[3] = { 1.0 / 255.0, 1.0 / 255.0 , 1.0 / 255.0 };
+			ncnn::Mat ncnn_in = ncnn::Mat::from_pixels(input_tile.data, ncnn::Mat::PIXEL_BGR2RGB, input_tile_padding_w, input_tile_padding_h);
+			ncnn::Mat ncnn_out;
+			ncnn_in.substract_mean_normalize(0, norm_vals);
+			ncnn::Extractor extractor = model.create_extractor();
+			extractor.input("input", ncnn_in);
+			extractor.extract("output", ncnn_out);
+
+			cv::Mat output_tile = to_ocv(ncnn_out);
+			if (output_tile.cols != input_tile.cols * scale || output_tile.rows != input_tile.rows * scale) {
+				return std::make_pair(result, "result scale is not x4 scale");
+			}
+
+			// output rect area
+			int output_top = input_top * scale;
+			int output_bottom = input_bottom * scale;
+			int output_left = input_left * scale;
+			int output_right = input_right * scale;
+
+			int output_tile_top = (input_top - input_top_padding) * scale;
+			int output_tile_bottom = output_tile_top + input_tile_h * scale;
+			int output_tile_left = (input_left - input_left_padding) * scale;
+			int output_tile_right = (output_tile_left + input_tile_w * scale);
+			
+			cv::Rect tile_rect = cv::Rect(output_tile_left, output_tile_top,
+				output_tile_right - output_tile_left,
+				output_tile_bottom - output_tile_top);
+
+			cv::Rect out_rect = cv::Rect(output_left, output_top,
+				output_right - output_left,
+				output_bottom - output_top);
+
+			output_tile(tile_rect).copyTo(result(out_rect));
+		}
+
+	return std::make_pair(result, no_error);
+}
