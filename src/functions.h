587de74cc94ab072522f7a61fd074d9932cd5bec
Kasai Kou <streamwest1629@gmail.com>
2023-01-06 13:01:10 +0900

    #19: rename function name

--- src/functions.h
+++ src/functions.h
@@ -7,8 +7,8 @@
 
 using ErrorMsg = std::string;
 
-cv::Mat to_cvmat(ARGB* image, const int& width, const int& height, const bool& monochrome = false) noexcept;
-cv::Mat to_cvmat(ARGB* image, const int& width, const int& height, const RectArea& padding, const bool& monochrome = false) noexcept;
+cv::Mat color_to_cvmat(ARGB* image, const int& width, const int& height, const bool& monochrome = false) noexcept;
+cv::Mat color_to_cvmat(ARGB* image, const int& width, const int& height, const RectArea& padding, const bool& monochrome = false) noexcept;
 void assign_cvmat(ARGB* image, const int& width, const int& height, cv::Mat mat);
 
 
