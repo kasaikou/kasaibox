60a7ba4536a59e764c6719d2836aed47dd6f3832
Kasai Kou <streamwest1629@gmail.com>
2023-01-07 12:41:40 +0900

    #19: cv::Matのコンストラクタでミスがあったので修正

--- src/opencv_functions.cpp
+++ src/opencv_functions.cpp
@@ -4,12 +4,12 @@ cv::Mat color_to_cvmat(ARGB* image, const int& width, const int& height, const b
 
 	if (monochrome) {
 	} else {
-		cv::Mat rgb = cv::Mat::zeros(width, height, CV_8UC3);
+		cv::Mat rgb = cv::Mat::zeros(height, width, CV_8UC3);
 		for (int y = 0; y < height; ++y)
 			for (int x = 0; x < width; ++x) {
-				rgb.at<cv::Vec3b>(x, y)[0] = uchar(image[x + y * width].b);
-				rgb.at<cv::Vec3b>(x, y)[1] = uchar(image[x + y * width].g);
-				rgb.at<cv::Vec3b>(x, y)[2] = uchar(image[x + y * width].r);
+				rgb.at<cv::Vec3b>(y, x)[0] = uchar(image[x + y * width].b);
+				rgb.at<cv::Vec3b>(y, x)[1] = uchar(image[x + y * width].g);
+				rgb.at<cv::Vec3b>(y, x)[2] = uchar(image[x + y * width].r);
 			}
 		return rgb;
 	}
@@ -19,12 +19,12 @@ cv::Mat color_to_cvmat(ARGB* image, const int& width, const int& height, const R
 	if (monochrome) {
 
 	} else {
-		cv::Mat rgb = cv::Mat::zeros(width - (padding.left + padding.right), height - (padding.top + padding.bottom), CV_8UC3);
+		cv::Mat rgb = cv::Mat::zeros(height - (padding.top + padding.bottom), width - (padding.left + padding.right), CV_8UC3);
 		for (int y_img = padding.top, h_img = height - padding.bottom, y_mat = 0; y_img < h_img; ++y_img, ++y_mat)
 			for (int x_img = padding.left, w_img = width - padding.right, x_mat = 0; x_img < w_img; ++x_img, ++x_mat) {
-				rgb.at<cv::Vec3b>(x_mat, y_mat)[0] = uchar(image[x_img + y_img * width].b);
-				rgb.at<cv::Vec3b>(x_mat, y_mat)[1] = uchar(image[x_img + y_img * width].g);
-				rgb.at<cv::Vec3b>(x_mat, y_mat)[2] = uchar(image[x_img + y_img * width].r);
+				rgb.at<cv::Vec3b>(y_mat, x_mat)[0] = uchar(image[x_img + y_img * width].b);
+				rgb.at<cv::Vec3b>(y_mat, x_mat)[1] = uchar(image[x_img + y_img * width].g);
+				rgb.at<cv::Vec3b>(y_mat, x_mat)[2] = uchar(image[x_img + y_img * width].r);
 			}
 		return rgb;
 	}
@@ -34,12 +34,12 @@ cv::Mat just_alpha_to_cvmat(ARGB* image, const int& width, const int& height, co
 	if (monochrome) {
 
 	} else {
-		cv::Mat rgb = cv::Mat::zeros(width - (padding.left + padding.right), height - (padding.top + padding.bottom), CV_8UC3);
+		cv::Mat rgb = cv::Mat::zeros(height - (padding.top + padding.bottom), width - (padding.left + padding.right), CV_8UC3);
 		for (int y_img = padding.top, h_img = height - padding.bottom, y_mat = 0; y_img < h_img; ++y_img, ++y_mat)
 			for (int x_img = padding.left, w_img = width - padding.right, x_mat = 0; x_img < w_img; ++x_img, ++x_mat) {
-				rgb.at<cv::Vec3b>(x_mat, y_mat)[0] = uchar(image[x_img + y_img * width].a);
-				rgb.at<cv::Vec3b>(x_mat, y_mat)[1] = uchar(image[x_img + y_img * width].a);
-				rgb.at<cv::Vec3b>(x_mat, y_mat)[2] = uchar(image[x_img + y_img * width].a);
+				rgb.at<cv::Vec3b>(y_mat, x_mat)[0] = uchar(image[x_img + y_img * width].a);
+				rgb.at<cv::Vec3b>(y_mat, x_mat)[1] = uchar(image[x_img + y_img * width].a);
+				rgb.at<cv::Vec3b>(y_mat, x_mat)[2] = uchar(image[x_img + y_img * width].a);
 			}
 		return rgb;
 	}
@@ -51,17 +51,17 @@ void assign_cvmat(ARGB* image, const int& width, const int& height, cv::Mat mat)
 	case CV_8UC3:
 		for (int y = 0; y < height; ++y)
 			for (int x = 0; x < width; ++x) {
-				image[x + y * width].b = std::byte(mat.at<cv::Vec3b>(x, y)[0]);
-				image[x + y * width].g = std::byte(mat.at<cv::Vec3b>(x, y)[1]);
-				image[x + y * width].r = std::byte(mat.at<cv::Vec3b>(x, y)[2]);
+				image[x + y * width].b = std::byte(mat.at<cv::Vec3b>(y, x)[0]);
+				image[x + y * width].g = std::byte(mat.at<cv::Vec3b>(y, x)[1]);
+				image[x + y * width].r = std::byte(mat.at<cv::Vec3b>(y, x)[2]);
 			}
 		return;
 	case CV_8UC1:
 		for (int y = 0; y < height; ++y)
 			for (int x = 0; x < width; ++x) {
-				image[x + y * width].b = std::byte(mat.at<cv::Vec<uchar, 1>>(x, y)[0]);
-				image[x + y * width].g = std::byte(mat.at<cv::Vec<uchar, 1>>(x, y)[0]);
-				image[x + y * width].r = std::byte(mat.at<cv::Vec<uchar, 1>>(x, y)[0]);
+				image[x + y * width].b = std::byte(mat.at<cv::Vec<uchar, 1>>(y, x)[0]);
+				image[x + y * width].g = std::byte(mat.at<cv::Vec<uchar, 1>>(y, x)[0]);
+				image[x + y * width].r = std::byte(mat.at<cv::Vec<uchar, 1>>(y, x)[0]);
 			}
 		return;
 	default:
