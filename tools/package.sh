d6ce6541154176a7ea5df6f439a188d6b3757da6
Kasai Kou <streamwest1629@gmail.com>
2023-01-07 15:29:43 +0900

    #19: 頒布用ライブラリを追加

--- tools/package.sh
+++ tools/package.sh
@@ -7,6 +7,7 @@ mkdir ./kasaibox
 mkdir ./kasaibox/dependencies
 cp ./libs/bin/opencv_world460.dll \
     ./libs/bin/ncnn.dll \
+    ./libs/bin/vulkan-1.dll \
     ./kasaibox/dependencies
 
 cp ./Release/kasaibox.dll \
